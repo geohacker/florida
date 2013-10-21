8c3d18e5de11b32919fc853542eb66aaf81c68b9
Sajjad Anwar <me@sajjad.in>
2013-10-21 19:54:54 +0530

    Update infobox

--- js/infographic.js
+++ js/infographic.js
@@ -13,20 +13,30 @@ d3.json("data/regions.json", function(json) {
 svg.selectAll("path")
 .data(json.features)
 .enter().append("path")
-.attr("d", path) });
+.attr("d", path)
+.on("mouseover", mouseover)
+});
 
 d3.select('#circuits').on('click', changeGeo);
 d3.select('#regions').on('click', changeGeo);
 d3.select('#agencies').on('click', changeGeo);
 
 function changeGeo() {
+	$(this).parent().addClass('active');
+	$(this).parent().siblings().removeClass('active');
 	geoType = d3.select(this).attr('id').substring(0);
 	d3.selectAll("path").remove();
 		d3.json("data/"+geoType+".json", function(json) {
 		svg.selectAll("path")
 		.data(json.features)
 		.enter().append("path")
-		.attr("d", path);
+		.attr("d", path)
+		.on("mouseover", mouseover);
 	});
+}
+
+function mouseover(d) {
+	d3.select('#name').text('ID - '+d.properties.ID);
+	// console.log(d);
 }
 
