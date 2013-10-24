b3658b0fae6e9ef50d8af5b69f72cc427eecaf9f
Sajjad Anwar <me@sajjad.in>
2013-10-24 11:09:30 +0530

    Pending interaction and inline documentation

--- js/infographic.js
+++ js/infographic.js
@@ -1,3 +1,6 @@
+// The following line initialize the map
+// And also setup how it is displayed.
+
 width = $('#map').width();
 var xy = d3.geo.mercator()
 .center([-83.393,28.076])
@@ -5,21 +8,50 @@ var xy = d3.geo.mercator()
 .translate([width/2, 500/2]);
 var path = d3.geo.path().projection(xy);
 
+// Set the default active geography as the region.
 var activeGeo = 'Region';
+
+// Set the default active KPI as KPI 1
+var activeKpi = 'kpi1'
 
 var svg = d3.select("#map")
 .append("svg")
 .attr("class", "shapes");
+
+// Load the region map data.
 
 d3.json("data/region.json", function(json) {
 svg.selectAll("path")
 .data(json.features)
 .enter().append("path")
 .attr("d", path)
+// Uncomment the following line to change the color of the 
+// shapes based on the data. You have also update the variable
+// names in the function called 'quantize'.
+// .attr("class", data ? quantize : null)
 .on("mouseover", mouseover)
 .on("mouseout", mouseout);
 
+// Uncomment the following line to add click interaction to
+// the shapes. You have to uncomment the 'click' function and
+// write the required action there.
+// .on("click", function(d) {clicked(d, this); });
 });
+
+// The following function is used to import JSON data.
+// Give the correct path to the file and the data will be available in 'data'.
+// d3.json("path to JSON data file", function(json) {
+// 	data = json;
+// });
+
+// The following function is used to import CSV data.
+// Give the correct path to the file and the data will be available in 'csv_data'.
+// d3.csv("path to CSV data file", function(csv) {
+//     csv_data = csv;
+//  });
+
+// Following lines for changing the geo type when the user
+// clicks the links in the header. 
 
 d3.select('#circuit').on('click', changeGeo);
 d3.select('#region').on('click', changeGeo);
@@ -52,3 +84,26 @@ function mouseout(d) {
 	d3.select('#name').classed('hide', true);
 }
 
+// function quantize(d) {
+// 	shape_data = data[d.properties['code']]; //replace code with the field where the ID is.
+//     color = "q" + Math.min(7, ~~(shape_data[0]['variable'] / 20)) + "-9"; //replace variable with the field where the value is.
+//     return color;
+//   }
+
+// function clicked (argument) {
+// 	// body...
+// }
+
+// The following lines are for actions when the KPI buttons are clicked.
+// The load the required data and redraw the charts in the function below.
+
+d3.selectAll('button').on('click', changeKPI);
+
+function changeKPI() {
+	d3.selectAll('button').classed('active', false);
+	d3.select(this).classed('active', true);
+	kpi = d3.select(this).text().toLowerCase().replace(/\s/g, '');
+	// the 'kpi' variable gives the KPI clicked just now.
+	// Load data.
+	// Redraw chars.
+}
